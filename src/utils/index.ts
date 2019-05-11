73eab3b219b502c7ef2bda176a87310e10a791f9
SuuQi <646382806@qq.com>
2019-05-11 22:14:38 +0800

    错别字

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,7 +6,7 @@ export * from './actionUtils'
 export * from './reducerUtils'
 
 export async function login () {
-    Taro.showLoading({ title: '登陆中...' })
+    Taro.showLoading({ title: '登录中...' })
     const loginOriginInfo = await Taro.login()
     const loginInfo = await store.dispatch( fetchLoginInfo(loginOriginInfo.code) as any )
     await Taro.setStorage({ key: SESSION_KEY, data: loginInfo.sessionId })
